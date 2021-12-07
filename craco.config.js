b17d86f93afa2a588db4080e952ef15f2e793c60
Derek Robati <derek.robati@gmail.com>
2021-12-07 17:52:59 -0500

    Initial work.

---
+++ craco.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    webpack: {
+        configure: {
+            module: {
+                rules: [
+                    {
+                        type: 'javascript/auto',
+                        test: /\.mjs$/,
+                        include: /node_modules/,
+                    },
+                ],
+            },
+        },
+    },
+    style: {
+        postcss: {
+            plugins: [
+                require('tailwindcss'),
+                require('autoprefixer'),
+            ],
+        },
+    },
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import { motion } from 'framer-motion';
+
+import Logo from "./Logo"
 import './App.css';
 
 function App() {
+  const colors = {
+    motionMagenta: "#f08",
+    motionBlue: "#05f",
+    motionPurple: "#70f",
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <Logo className="App-logo" />
+        <motion.p
+            initial={{ opacity: 1 }}
+            animate={{ opacity: 0 }}
+            transition={{
+              delay: 4,
+              duration: 2,
+            }}
+        >
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        </motion.p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +33,28 @@ function App() {
         >
           Learn React
         </a>
+        <motion.a
+          style={{
+              background: `linear-gradient(to right,
+                      ${colors.motionBlue} 20%,
+                      ${colors.motionMagenta}, 
+                      ${colors.motionPurple})`,
+              WebkitBackgroundClip: "text",
+              WebkitTextFillColor: "transparent",
+              textDecorationColor: colors.motionPurple,
+              marginTop: 5,
+          }}
+          href="https://www.framer.com/api/motion/"
+          target="_blank"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{
+              delay: 4,
+              duration: 2,
+          }}
+        >
+          & Framer Motion
+        </motion.a>
       </header>
     </div>
   );
